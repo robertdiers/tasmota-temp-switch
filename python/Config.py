4d484fdf7bdab0e9b618e4d51e41dd517f45fd50
Robert Diers <robertdiers@gmail.com>
2023-01-28 17:51:16 +0100

    reduced log

--- python/Config.py
+++ python/Config.py
@@ -22,28 +22,28 @@ def read():
         values["mqtt_name"] = config['MqttSection']['mqtt_name']
         if os.getenv('MQTT_BROKER','None') != 'None':
             values["mqtt_broker"] = os.getenv('MQTT_BROKER')
-            print ("using env: MQTT_BROKER")
+            #print ("using env: MQTT_BROKER")
         if os.getenv('MQTT_PORT','None') != 'None':
             values["mqtt_port"] = int(os.getenv('MQTT_PORT'))
-            print ("using env: MQTT_PORT")
+            #print ("using env: MQTT_PORT")
         if os.getenv('MQTT_USER','None') != 'None':
             values["mqtt_user"] = os.getenv('MQTT_USER')
-            print ("using env: MQTT_USER")
+            #print ("using env: MQTT_USER")
         if os.getenv('MQTT_PASSWORD','None') != 'None':
             values["mqtt_password"] = os.getenv('MQTT_PASSWORD')
-            print ("using env: MQTT_PASSWORD")
+            #print ("using env: MQTT_PASSWORD")
         if os.getenv('MQTT_NAME','None') != 'None':
             values["mqtt_name"] = os.getenv('MQTT_NAME')
-            print ("using env: MQTT_NAME")
+            #print ("using env: MQTT_NAME")
         
         values["temp_on"] = config['TempSection']['temp_on']
         values["temp_off"] = int(config['TempSection']['temp_off'])
         if os.getenv('TEMP_ON','None') != 'None':
             values["temp_on"] = os.getenv('TEMP_ON')
-            print ("using env: TEMP_ON")
+            #print ("using env: TEMP_ON")
         if os.getenv('TEMP_OFF','None') != 'None':
             values["temp_off"] = int(os.getenv('TEMP_OFF'))
-            print ("using env: TEMP_OFF")
+            #print ("using env: TEMP_OFF")
         
         #print (datetime.now().strftime("%d/%m/%Y %H:%M:%S") + " config: ", values)
 
